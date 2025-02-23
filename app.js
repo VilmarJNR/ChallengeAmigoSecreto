973e374abe7ccdf640a6d7a65e9def3a209f294d
Vilmar Rodrigues de Vasconcelos Júnior <134446565+VilmarJNR@users.noreply.github.com>
2025-02-23 20:14:19 -0300

    Função adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Lista que ficara os nomes de amigos
+let amigos = [];
+
+//função para adicionar um amigo
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nomeAmigo = inputAmigo.value.trim();
+
+    //validar se o campo esta vazio ou tem apenas espaços em branco
+    if (nomeAmigo === "") {
+        alert("Por favor, coloque um Nome");
+        return;
+    }
+
+    //validar se o nome ja esta na lista
+    if (amigos.includes(nomeAmigo)) {
+        alert(`O nome ${nomeAmigo} já está na lista, na posição ${amigos.indexOf(nomeAmigo) + 1}.`);
+        return;
+    }
+
+    //Adicionar o amigo na lista
+    amigos.push(nomeAmigo);
+
+    //limpar o campo de adicionar o nome
+    inputAmigo.value = "";
+
+    //Atualizar a lista
+    atualizarLista();
+
+    //focar no campo de entrada
+    inputAmigo.focus();
+}
